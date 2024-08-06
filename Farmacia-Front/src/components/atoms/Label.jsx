39566be7874acd187be3f08634f8c5666814c7f0
AngelVR08 <160242042+AngelVR08@users.noreply.github.com>
2024-08-05 19:24:39 -0600

    Cambiando la ubicacion de archivos

--- Farmacia-Front/src/components/atoms/Label.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Label = ({ text, htmlFor }) => {
-  return (
-    <label htmlFor={htmlFor}>{text}</label>
-  );
-};
-
-export default Label;
--- Farmacia-Front/src/components/atoms/Title.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Title = ({ children }) => {
-  return <h2>{children}</h2>;
-};
-
-export default Title;
